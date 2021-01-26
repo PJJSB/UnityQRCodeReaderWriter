d39bd63ab2211cd41a2e02792f4d234e1642b4ae
Pieter <pieter@hellobuurman.nl>
2021-01-26 16:46:40 +0100

    Add project

---
+++ QRCodeReader/Assets/Scripts/CreateQRCode.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using System.Drawing;
+using System;
+using UnityEngine.UI;
+using System.IO;
+
+public class CreateQRCode : MonoBehaviour
+{
+    //public MeshRenderer material;
+    //public GameObject plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+
+        for (int i = 0; i < 500; i++)
+        {
+            SaveTextureToFile(GenerateQRCode(i + ""), "QRcode"+i);
+        }
+
+        watch.Stop();
+        Debug.LogFormat("watch.ElapsedMilliseconds: {0}", watch.ElapsedMilliseconds);
+    }
+
+    private void SaveTextureToFile(Texture2D texture2D, string name)
+    {
+        byte[] bytes = texture2D.EncodeToJPG();
+        File.WriteAllBytes(Application.dataPath + "/QRcodes~/" + name + ".jpg", bytes);
+    }
+
+    Texture2D GenerateQRCode(string text)
+    {
+        IBarcodeWriter writer = new BarcodeWriter { Format = BarcodeFormat.QR_CODE };
+        var result = writer.Write(text);
+        var root = (int)Mathf.Sqrt(result.Length);
+        Texture2D qrCode = new Texture2D(root, root);
+
+        for (int y = 0; y < qrCode.height; y++)
+        {
+            for (int x = 0; x < qrCode.width; x++)
+            {
+                qrCode.SetPixel(x, y, result[qrCode.width * y + x]);
+            }
+        }
+
+        qrCode.Apply();
+
+        return qrCode;
+    }
+}
---
+++ QRCodeReader/Assets/Scripts/ReadQRCode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+public class ReadQRCode : MonoBehaviour
+{
+    private WebCamTexture camTexture;
+    private Rect screenRect;
+    private Coroutine readValue = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTexture = new WebCamTexture();
+        camTexture.requestedHeight = 480;
+        camTexture.requestedWidth = 360;
+        if (camTexture != null)
+        {
+            camTexture.Play();
+        }
+    }
+
+    void OnGUI()
+    {
+        // drawing the camera on screen
+        GUIUtility.RotateAroundPivot(90f, new Vector2(Screen.width/2, Screen.height/2));
+        GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+        
+        if (readValue == null)
+        {
+            StartCoroutine("ReadValue");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator ReadValue()
+    {
+        try
+        {
+            IBarcodeReader barcodeReader = new BarcodeReader();
+            // decode the current frame
+            var result = barcodeReader.Decode(camTexture.GetPixels32(), camTexture.width, camTexture.height);
+
+            if (result != null)
+            {
+                //Doe hier iets met de decoded QR-code
+                Debug.Log("DECODED TEXT FROM QR: " + result.Text);
+            } else
+            {
+                Debug.LogFormat("decode was null");
+            }
+        } catch (Exception ex)
+        {
+            Debug.LogWarning(ex.Message);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        readValue = null;
+    }
+}
